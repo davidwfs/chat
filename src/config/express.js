ad7eb5a91cee13ba24d56ac1840b32baedb61445
David Washington <davidwashington833@gmail.com>
2019-06-15 21:07:30 -0300

    Config socket.io

--- src/config/express.js
+++ src/config/express.js
@@ -1,14 +1,21 @@
+import http from 'http';
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import cors from 'cors';
+import socket from 'socket.io';
 
 import log from '../services/winston';
-
 import value from '../routes/value';
 
 const app = express();
+const server = http.Server(app);
+const io = socket(server);
 
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
@@ -18,4 +25,4 @@ app.use(morgan('combined', {
 
 app.use('/value', value);
 
-export default app;
+export default server;
--- src/controllers/value.js
+++ src/controllers/value.js
@@ -1,6 +1,8 @@
 export default {
 
   get(req, res) {
+    const { io } = req;
+    io.emit('hi');
     res.status(200).send(['v1', 'v2', 'v3']);
   },
 
