d76c4d15f49f779a15046b4fdd73c5bc05906770
David Washington <davidwashington833@gmail.com>
2019-06-15 22:44:37 -0300

    List, update and submit message in client

--- src/public/assets/js/main.js
+++ src/public/assets/js/main.js
@@ -1,15 +1,43 @@
 const select = document.querySelector.bind(document);
+const resToJSON = res => res.json();
 
-function listMsgs(msgs) {
-  const boxMgsgs = select('#box-msg');
-  const html = msgs.map(msg => `<p>${msg}</p>`).join('');
+function showMessage(message) {
+  const listMessages = select('#list-messages');
+  const element = document.createElement('li');
+  element.textContent = message;
+  listMessages.appendChild(element);
+}
 
-  boxMgsgs.innerHTML = html;
+const showMessages = messages => messages.forEach(showMessage);
+
+function submitMsg(event) {
+  event.preventDefault();
+  const form = event.target;
+  const { value: message } = form.querySelector('[name="message"]');
+  const headers = new Headers({
+    'Content-Type': 'application/json; charset=utf-8',
+  });
+
+  fetch('/message', {
+    headers,
+    method: 'POST',
+    body: JSON.stringify({ message }),
+  }).catch(err => console.log(err));
+}
+
+function loadMessages() {
+  fetch('/message')
+    .then(resToJSON)
+    .then(showMessages)
+    .catch(err => console.log(err));
 }
 
 window.addEventListener('load', () => {
-  fetch('/value')
-    .then(res => res.json())
-    .then(listMsgs)
-    .catch(err => console.log(err));
+  const formChat = select('#form-message');
+  const socket = io.connect('http://localhost:5000');
+
+  socket.on('postMessage', data => showMessage(data));
+  formChat.addEventListener('submit', submitMsg);
+
+  loadMessages();
 });
